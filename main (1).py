918bd3e8ecfa65c6081ba3bc6198616ff196e4f0
gamil1025 <97983697+gamil1025@users.noreply.github.com>
2022-01-18 22:16:26 +0300

    Add files via upload
    make with love:)
    its Beta 1 soon update

---
+++ main (1).py
@@ -0,0 +1,31 @@
+print('Welcome to THE casino')
+
+print('Press any button to start!')
+
+print('Find one hundred for Win!')
+
+print('The letters:a b c d e f g h z w')
+
+a=10 
+
+b=20
+
+c=100 
+
+d=50
+
+e=60
+
+w=70
+
+g=30
+
+h=40
+
+i=80
+
+z=90
+
+stop(all)
+
+# its a beta 1
